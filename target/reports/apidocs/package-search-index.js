d5a846e163ddbf2396bef9d3f27473db0e4ee1e4
Salvatore <salvatorevigano9331@gmail.com>
2025-03-18 17:10:53 +0100

    Aggiunto target per javadoc

---
+++ target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.music.app"},{"l":"com.example.music.config"},{"l":"com.example.music.dao"},{"l":"com.example.music.model"}];updateSearchResults();
